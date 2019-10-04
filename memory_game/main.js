08931badc54639c4a87a27f4003a2f069d2c3bc6
Sarah Wood <sarahwood@Sarahs-MacBook-Pro.local>
2019-10-04 11:50:56 +0100

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,7 +1,9 @@
+console.log("Up and running!");
+
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-console.log("User flipped " + cardOne)
-console.log("User flipped " + cardFour)
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
